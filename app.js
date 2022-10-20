6c8946a2642207984d496a897579bce9bedcfe58
Ezechiel Iti <papaezechiel@sympatico.ca>
2022-10-20 17:44:46 -0400

    DB Connection

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+
+const app = express();
+
+const mongoose = require("mongoose");
+require("dotenv/config");
+// Middleware ( a function who is execute when the route is execute)
+
+// app.use('/posts', () => {
+//   console.log('This is a middleware used in post')
+// })
+
+// for the authentication every time any rout is execute, he gonna check the authentication
+// app.use(auth)
+
+// ROUTES
+app.get("/", (req, res) => {
+  res.send("We are on home");
+});
+
+app.get("/posts", (req, res) => {
+  res.send("We are on posts");
+});
+
+// Connect to DB
+
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, () => {
+  console.log("Connected to DB");
+});
+//How do we start listening to the server
+app.listen(3000);
